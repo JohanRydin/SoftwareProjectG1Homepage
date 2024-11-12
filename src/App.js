a3bc431c643d078db93ae2d9858303134a10e82a
JohanRydin <johanrydin00@gmail.com>
2024-11-12 11:23:40 +0100

    filip team leader

--- src/App.js
+++ src/App.js
@@ -5,45 +5,57 @@ import LawendImage from './pictures/Lawend.JPG';
 import ErdenImage from './pictures/Erden.jpg';
 import IsacImage from './pictures/Isac.JPG';
 
-function Profile({ name, email, image }) {
+function Profile({ name, email, leader, image }) {
   return (
     <div className='profile'>
       <img src={image} alt={name}></img>
       <p className='profile-name'>{name}</p>
       <p className='profile-email'>{email}</p>
+      {leader && <p className='leader'>Project Manager</p>}
     </div>
   )
 }
 
 const members = [
   {
+    name: "Filip Eriksson",
+    email: "filip.eriksson4@outlook.com",
+    image: FilipImage,
+    isLeader: true,
+
+
+  },
+  {
     name: "Johan Rydin",
     email: "johanrydin00@gmail.com",
     image: johanImage,
-  },
-  {
-    name: "Filip Eriksson",
-    email: "filip.eriksson4@outlook.com",
-    image: FilipImage
+    isLeader: false
+
   },
   {
     name: "Lawend Mardini",
     email: "mardini.lawend@gmail.com",
     image: LawendImage,
+    isLeader: false,
+
   },
   {
     name: "Erdem Garip",
     email: "erdemg0919@gmail.com",
     image: ErdenImage,
+    isLeader: false,
+
   },
   {
     name: "Isac Arrhenius",
     email: "isac.arrhenius@gmail.com",
     image: IsacImage,
+    isLeader: false,
+
   },
 ]
 
-const profile_members = members.map(({ name, email, image }) => (<Profile name={name} email={email} image={image} />));
+const profile_members = members.map(({ name, email, isLeader, image }) => (<Profile name={name} email={email} leader={isLeader} image={image} />));
 
 function App() {
   return (
